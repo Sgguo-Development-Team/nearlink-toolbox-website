00c5f2865be937faf1b74b114b73384970770528
传说当中的帅锅（The Legendary Handsome Pot） <94915862+ChuanShuoShuai@users.noreply.github.com>
2026-02-06 17:09:48 +0800

    Add files via upload

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -36,7 +36,7 @@ export default function Footer() {
             Built & Designed by <a href="https://www.gcxstudio.cn" className="bg-gradient-to-r from-purple-400 to-pink-600 bg-clip-text text-transparent font-bold hover:from-pink-600 hover:to-purple-400 transition duration-500">
               MiraHikari
             </a> with 👌<br></br>
-域名备案：<a href="https://beian.miit.gov.cn/#/Integrated/index" className="hover:text-foreground transition-colors">闽ICP备2026003001号-1</a> <a href="https://beian.mps.gov.cn/#/query/webSearch?code=35010302000932" className="hover:text-foreground transition-colors"><img src="https://p4.ssl.qhimg.com/t110b9a93011feaa2d89602ba00.webp"></img>公安网监闽公网安备35010302000932号</a>
+域名备案：<a href="https://beian.miit.gov.cn/#/Integrated/index" className="hover:text-foreground transition-colors">闽ICP备2026003001号-1</a> <a href="https://beian.mps.gov.cn/#/query/webSearch?code=35010302000932" className="hover:text-foreground transition-colors">公安网监闽公网安备35010302000932号</a>
           </p>
           © 2024-PRESENT <a href="https://www.sgguo.com" className="hover:text-foreground transition-colors">Sgguo Development Team</a>. 保留所有权利。
         </div>
